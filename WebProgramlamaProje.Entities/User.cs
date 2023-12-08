f41133f87a5c8b31d04d711f4243c488f4b5a7f5
candartozan <candartozan@gmail.com>
2023-12-08 21:11:37 +0300

    Kullanıcı ve Rol sınıfı eklendi.

---
+++ WebProgramlamaProje.Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProje.Entities
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public int RoleId { get; set; }
+
+		[Required(ErrorMessage = "Ad alanı boş olamaz.")]
+		public String Name { get; set; }
+
+		[Required(ErrorMessage = "Soyad alanı boş olamaz.")]
+		public String Surname { get; set; }
+
+		[Required(ErrorMessage = "Eposta boş olamaz.")]
+		public String Email { get; set; }
+
+		[Required(ErrorMessage = "Şifre boş olamaz.")]
+		public String Password { get; set; }
+		public String PhoneNumber { get; set; }
+		public DateTime DateOfRegistration { get; set; }
+		public virtual Role Role { get; set; }
+	}
+}

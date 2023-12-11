e05bf74b31fa347ef1157532f1c8424748af58a2
candartozan <candartozan@gmail.com>
2023-12-11 15:57:30 +0300

    Kullanıcı giriş sayfası tasarlandı.

---
+++ WebProgramlamaProje.MVC/Models/UserLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebProgramlamaProje.MVC.Models
+{
+	public class UserLoginViewModel
+	{
+		[Required(ErrorMessage = "Eposta giriş için gereklidir.")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Şifre giriş gereklidir.")]
+		public string Password { get; set; }
+	}
+}

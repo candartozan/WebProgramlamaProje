bd4d12a3c49d83441e911af51da9da4b7c36cea2
candartozan <candartozan@gmail.com>
2023-12-12 18:03:31 +0300

    İlçe, il ve bölge veri tabanına eklendi.

--- WebProgramlamaProje.DataAccess/Concrete/Context.cs
+++ WebProgramlamaProje.DataAccess/Concrete/Context.cs
@@ -17,8 +17,11 @@ namespace WebProgramlamaProje.DataAccess.Concrete
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<Role> Roles { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Brand> Brands { get; set; }
-        public DbSet<Product> Products { get; set; }
-    }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<District> Districts { get; set; }
+		public DbSet<City> Cities { get; set; }
+		public DbSet<Region> Regions { get; set; }
+	}
 }

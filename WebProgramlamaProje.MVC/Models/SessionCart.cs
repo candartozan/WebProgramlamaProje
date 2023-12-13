d23f9318b35325af2869cbdff206464939b3f533
candartozan <candartozan@gmail.com>
2023-12-13 18:19:36 +0300

    SessionCart Eklendi

--- WebProgramlamaProje.MVC/Models/SessionCart.cs
+++ WebProgramlamaProje.MVC/Models/SessionCart.cs
@@ -6,7 +6,61 @@ using WebProgramlamaProje.Entities;
 
 namespace WebProgramlamaProje.MVC.Models
 {
-	public static class  SessionCart
+	public static class SessionCart
 	{
+		public static Cart GetCart()
+		{
+			var context = HttpContext.Current;
+			if (string.IsNullOrEmpty(Convert.ToString(context.Session["Cart"])))
+			{
+				Cart newCart = new Cart();
+				context.Session["Cart"] = newCart;
+				return newCart;
+			}
+
+			var cart = (Cart)context.Session["Cart"];
+			return cart;
+		}
+
+		public static void AddProductToCart(Product product)
+		{
+			var context = HttpContext.Current;
+			var cart = (Cart)context.Session["Cart"];
+
+			var cartItem = cart.CartItems.FirstOrDefault(ci => ci.ProductId == product.Id);
+
+			if (cartItem != null)
+			{
+				cartItem.Quantity++;
+				context.Session["Cart"] = cart;
+			}
+			else
+			{
+				cart.CartItems.Add(new CartItem { ProductId = product.Id, Product = product, Quantity = 1 });
+				context.Session["Cart"] = cart;
+			}
+		}
+
+		public static void ClearCart()
+		{
+			var context = HttpContext.Current;
+			var cart = (Cart)context.Session["Cart"];
+
+			cart.CartItems.Clear();
+			context.Session["Cart"] = cart;
+		}
+
+		public static void RemoveProductFromCart(int id)
+		{
+			var context = HttpContext.Current;
+			var cart = (Cart)context.Session["Cart"];
+
+			cart.CartItems.Remove(
+				cart
+				.CartItems
+				.FirstOrDefault(ci => ci.ProductId == id));
+
+			context.Session["Cart"] = cart;
+		}
 	}
 }

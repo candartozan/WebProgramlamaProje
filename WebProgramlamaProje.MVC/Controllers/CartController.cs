d175d1e45c8361b87be8fa665e309c8bc9361d04
candartozan <candartozan@gmail.com>
2023-12-13 19:00:56 +0300

    Sepet işlemleri yapıldı.

---
+++ WebProgramlamaProje.MVC/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProgramlamaProje.Business.Abstract;
+using WebProgramlamaProje.MVC.Models;
+
+namespace WebProgramlamaProje.MVC.Controllers
+{
+	public class CartController : Controller
+	{
+		private readonly IProductService _productService;
+
+		public CartController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+		// GET: Cart
+		public ActionResult Index()
+		{
+			var model = SessionCart.GetCart();
+
+			return View(model);
+		}
+
+		public ActionResult AddCart(int id)
+		{
+			var product = _productService.GetProductWithDetailsById(id);
+			SessionCart.AddProductToCart(product);
+			return RedirectToAction("Index", "Cart");
+		}
+
+		public ActionResult RemoveFromCart(int id)
+		{
+			SessionCart.RemoveProductFromCart(id);
+
+			return RedirectToAction("Index", "Cart");
+		}
+	}
+}

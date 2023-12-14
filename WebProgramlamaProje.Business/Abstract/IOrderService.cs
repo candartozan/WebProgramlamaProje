ab5b3b2c2fcdabca9241afede64a86688a77b7b9
candartozan <candartozan@gmail.com>
2023-12-14 15:04:13 +0300

    Şipariş manager yazıldı.

---
+++ WebProgramlamaProje.Business/Abstract/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProgramlamaProje.Entities;
+
+namespace WebProgramlamaProje.Business.Abstract
+{
+	public interface IOrderService
+	{
+		List<Order> GetAllOrders();
+		List<Order> GetUserOrders(int id);
+		void Add(Order order);
+		void UpdateOrder(Order order);
+		void DeleteOrder(int id);
+		List<CargoCompany> GetCargoCompanies();
+		List<OrderStatus> GetOrderStatuses();
+	}
+}
---
+++ WebProgramlamaProje.Business/Concrete/OrderManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProgramlamaProje.Business.Abstract;
+using WebProgramlamaProje.DataAccess.Abstract;
+using WebProgramlamaProje.Entities;
+
+namespace WebProgramlamaProje.Business.Concrete
+{
+	public class OrderManager : IOrderService
+	{
+		private readonly IOrderDal _orderDal;
+		private readonly ICargoCompanyDal _cargoCompanyDal;
+		private readonly IOrderStatusDal _orderStatusDal;
+
+		public OrderManager(IOrderDal orderDal)
+		{
+			_orderDal = orderDal;
+		}
+
+		public void Add(Order order)
+		{
+			_orderDal.Create(order);
+		}
+
+		public void DeleteOrder(int id)
+		{
+			var order = _orderDal.FindByCondition(o => o.Id == id);
+			_orderDal.Remove(order);
+		}
+
+		public List<Order> GetAllOrders()
+		{
+			var orders = _orderDal.FindAll().ToList();
+			foreach (var o in orders)
+			{
+				o.CargoCompany = _cargoCompanyDal.FindByCondition(cc => cc.Id == o.CargoCompanyId);
+				o.OrderStatus = _orderStatusDal.FindByCondition(os => os.Id == o.OrderStatusId);
+			}
+
+			return orders;
+		}
+
+		public List<CargoCompany> GetCargoCompanies()
+		{
+			return _cargoCompanyDal.FindAll().ToList();
+		}
+
+		public List<OrderStatus> GetOrderStatuses()
+		{
+			return _orderStatusDal.FindAll().ToList();
+		}
+
+		public List<Order> GetUserOrders(int id)
+		{
+			var orders = _orderDal.FindAll().Where(o => o.UserId == id).ToList();
+			foreach (var o in orders)
+			{
+				o.CargoCompany = _cargoCompanyDal.FindByCondition(cc => cc.Id == o.CargoCompanyId);
+				o.OrderStatus = _orderStatusDal.FindByCondition(os => os.Id == o.OrderStatusId);
+			}
+
+			return orders;
+		}
+
+		public void UpdateOrder(Order order)
+		{
+			_orderDal.Update(order);
+		}
+	}
+}
